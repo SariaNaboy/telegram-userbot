60063d05e038668b338385795606996f6ca0ed98
SariaNaboy <SariaNaboy@outlook.com>
2026-08-31 15:46:53 +0330

    bot3.py: revert profile name (change belongs to main.py only)

--- bot3.py
+++ bot3.py
@@ -134,7 +134,7 @@ class InputPrivacyKeyAbout(TLObject):
         return b.getvalue()
 
 
-PROFILE_AMIRALI_NAME = "𝑩𝒍𝒂𝒄𝒌 𝑳𝒖𝒏𝒈 𝑴𝒐𝒓𝒈𝒂𝒏"
+PROFILE_AMIRALI_NAME = "⒜⒨⒤⒭⒜⒧⒤"
 PROFILE_AMIRALI_USERNAME = "Amirali126868"
 PROFILE_MAYA_NAME = "Maya"
 PROFILE_MAYA_USERNAME = ""          # بدون یوزرنیم
